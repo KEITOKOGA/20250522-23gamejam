8a7f65b0360fbfb99ede35cb592d762b07d69e2c
マックス <maxoumixou@gmail.com>
2025-05-23 13:52:03 +0900

    InGameSceneのUIを追加

--- Assets/Max/Scripts/ExitButton.cs
+++ Assets/Max/Scripts/ExitButton.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class ExitButton : MonoBehaviour
 {
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainTitle");
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("InGameScene");
+    }
+
     public void Exit()
     {
         Application.Quit();
