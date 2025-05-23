12ffa1a5b032cea46c906aaca7bcf14484841500
KEITOKOGA <134474068+KEITOKOGA@users.noreply.github.com>
2025-05-23 12:39:55 +0900

    gitignore修正とゲージの色変更

--- Assets/Hyougo/GaugeController.cs
+++ Assets/Hyougo/GaugeController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GaugeController : MonoBehaviour
 {
@@ -8,15 +9,30 @@ public class GaugeController : MonoBehaviour
     [SerializeField] private GameObject _graceGauge;
     [SerializeField] private float _waitingTime;
     public int _HP;
+    public int _currentHP;
     private float _HP1;
 
+    private void Update()
+    {
+
+    }
     public void Init()
     {
+        _currentHP = _HP;
         _HP1 = _gauge.GetComponent<RectTransform>().sizeDelta.x / _HP;
     }
 
     public void BeInjured(int attack)
     {
+        _currentHP -= attack;
+        if (_currentHP <= _HP / 4)
+        {
+            _gauge.GetComponent<Image>().color = new Color(255, 0, 0);
+        }
+        else if (_currentHP <= _HP / 2)
+        {
+            _gauge.GetComponent<Image>().color = new Color(255, 165, 0);
+        }
         float damage = _HP1 * attack;
         StartCoroutine(damageEnumerator(damage));
     }
